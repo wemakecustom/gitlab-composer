429a347f90817605ed28f40ec94f61dcd8ba1ba7
Michal Gebauer <mishak@mishak.net>
2014-01-13 07:13:34 +0100

    Fix: $fetch_composer composer.json was not parsed

--- htdocs/packages.php
+++ htdocs/packages.php
@@ -53,7 +53,7 @@ $repos = $client->api('repositories');
 $fetch_composer = function($project, $ref) use ($repos) {
     try {
         $data = array();
-        $composer = $repos->blob($project['id'], $ref, 'composer.json');
+        $composer = json_decode($repos->blob($project['id'], $ref, 'composer.json'), true);
 
         if (empty($composer['name']) || $composer['name'] != $project['path_with_namespace']) {
             return false; // packages must have a name and must match
